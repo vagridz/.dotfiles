54b26a34c1c2d58ac1edfa40621020a678f2ff51
vagrid <vagrid@zaclys.net>
2025-04-24 20:37:37 +0200

    ✨ feat: add more neovim basic options on cursor/tab/indent

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -50,3 +50,36 @@ opt.number = true
 -- vim.cmd("set relativenumber")
 opt.relativenumber = true
 
+-- Set an 80 column border (horizontal line) for good coding style
+-- vim.cmd("set cc=80")
+opt.cc = "80"
+
+-- Width of a tab character in columns (2 spaces for tabs)
+-- vim.cmd("set tabstop=2")
+opt.tabstop = 2
+
+-- Amount of white spaces for level of indentation in columns
+-- vim.cmd("set shiftwidth=2")
+opt.shiftwidth = 2
+
+-- Set the number of columns for a tab character
+-- vim.cmd("set softtabstop=2")
+opt.softtabstop = 2
+
+-- Replace tabs by white space characters
+-- vim.cmd("set expandtab")
+opt.expandtab = true
+
+-- Insert blanks according to 'shiftwidth'.  
+-- <BS> (backspace) will delete a 'shiftwidth' worth of space 
+-- at the start of the line.
+opt.smarttab = true
+
+-- Use the syntax/style of code for indenting
+opt.smartindent = true
+
+-- Use indentation of the current line for the next one
+opt.autoindent = true
+
+-- Show line under cursor
+opt.cursorline = true
